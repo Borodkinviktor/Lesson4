74e489dde2b78b0cb33cddc1c57b76a3c40cc0da
borodkinviktor <borodkinviktor@gmail.com>
2023-04-10 21:35:36 +0300

    Домашняя работа-07

---
+++ src/loggers/ILogger.java
@@ -0,0 +1,5 @@
+package loggers;
+
+public interface ILogger {
+    public void log(String text);
+}
---
+++ src/model/IFileOperation.java
@@ -0,0 +1,6 @@
+package model;
+
+public interface IFileOperation {
+    String readText();
+    void saveText(String text);
+}
